2b0f845106bcffc966aea427d15dcc25614bbfcd
Kailash <137093796+kailzxh@users.noreply.github.com>
2025-05-25 20:46:12 +0530

    Time: 2 ms (55.93%), Space: 44.9 MB (9.69%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//        Stack<Integer> stack = new Stack<>();
+//        boolean[] visited = new boolean[rooms.size()];
+//         stack.push(0);
+
+//           while (!stack.isEmpty()) {
+//             int room = stack.pop();
+//             if (!visited[room]) {
+//                 visited[room] = true;
+//                 for (int key : rooms.get(room)) {
+//                     if (!visited[key]) {
+//                         stack.push(key);
+//                     }
+//                 }
+//             }
+//         }
+//         for (boolean v : visited) {
+//             if (!v) return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-       Stack<Integer> stack = new Stack<>();
-       boolean[] visited = new boolean[rooms.size()];
-        stack.push(0);
-
-          while (!stack.isEmpty()) {
-            int room = stack.pop();
-            if (!visited[room]) {
+        boolean[] visited = new boolean[rooms.size()];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        while(!q.isEmpty()){
+            int room =q.poll();
+           if (!visited[room]) {
                 visited[room] = true;
                 for (int key : rooms.get(room)) {
                     if (!visited[key]) {
-                        stack.push(key);
+                        q.offer(key);
                     }
                 }
             }
