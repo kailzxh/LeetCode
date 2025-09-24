777f8e06e99cb75d20feed1feeb18f9cb53686c4
Kailash <godarakailash741@gmail.com>
2025-09-24 14:45:48 +0530

    Time: 650 ms (99.91%), Space: 0B (100%) - LeetHub

---
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,14 @@
+WITH RunningTotal AS (
+    SELECT
+        person_id,
+        person_name,
+        weight,
+        turn,
+        SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+    FROM Queue
+)
+SELECT person_name
+FROM RunningTotal
+WHERE cumulative_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
