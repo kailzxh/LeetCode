6c91880b441978fbb4a7e874a196d45a9b3130ca
Kailash <godarakailash741@gmail.com>
2025-11-20 00:08:25 +0530

    Time: 1 ms (99.2%), Space: 46.4 MB (11.65%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -14,34 +14,36 @@
  * }
  */
 class Solution {
+
     public List<TreeNode> generateTrees(int n) {
         if (n == 0) return new ArrayList<>();
-        return buildTrees(1, n);
+        return build(1, n);
     }
 
-    private List<TreeNode> buildTrees(int start, int end) {
-        List<TreeNode> allTrees = new ArrayList<>();
-        
-        if (start > end) {
-            allTrees.add(null);
-            return allTrees;
-        }
-        for (int i = start; i <= end; i++) {
-            List<TreeNode> leftTrees = buildTrees(start, i - 1);
-           
-            List<TreeNode> rightTrees = buildTrees(i + 1, end);
+    private List<TreeNode> build(int start, int end) {
 
- 
+        List<TreeNode> result = new ArrayList<>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+        for (int rootVal = start; rootVal <= end; rootVal++) {
+
+            List<TreeNode> leftTrees = build(start, rootVal - 1);
+            List<TreeNode> rightTrees = build(rootVal + 1, end);
+
             for (TreeNode left : leftTrees) {
                 for (TreeNode right : rightTrees) {
-                    TreeNode root = new TreeNode(i);
+
+                    TreeNode root = new TreeNode(rootVal);
                     root.left = left;
                     root.right = right;
-                    allTrees.add(root);
+
+                    result.add(root);
                 }
             }
         }
 
-        return allTrees;
+        return result;
     }
 }
