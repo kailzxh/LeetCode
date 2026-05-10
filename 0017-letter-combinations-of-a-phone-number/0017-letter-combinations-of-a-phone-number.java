d51d3c6da58ecdbca5d18c14b1fa36cf2025cb4d
Kailash <godarakailash741@gmail.com>
2026-05-10 21:07:03 +0530

    Time: 3 ms (34.37%), Space: 43.6 MB (53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,30 +1,33 @@
-import java.util.*;
-
 class Solution {
-    private static final Map<Character, String> phoneMap = Map.of(
-        '2', "abc", '3', "def", '4', "ghi", '5', "jkl",
-        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz"
-    );
+        List<String> ans = new ArrayList<>();
+       Map<Integer, List<Character>> map = new HashMap<>();
 
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if (digits == null || digits.length() == 0) return result;
+        
+        map.put(2, new ArrayList<>(List.of('a', 'b', 'c')));
+        map.put(3, new ArrayList<>(List.of('d', 'e', 'f')));
+        map.put(4, new ArrayList<>(List.of('g', 'h', 'i')));
+        map.put(5, new ArrayList<>(List.of('j', 'k', 'l')));
+        map.put(6, new ArrayList<>(List.of('m', 'n', 'o')));
+        map.put(7, new ArrayList<>(List.of('p', 'q', 'r', 's')));
+        map.put(8, new ArrayList<>(List.of('t', 'u', 'v')));
+        map.put(9, new ArrayList<>(List.of('w', 'x', 'y', 'z')));
+        if(digits.length()==0){
+            return ans;
+        }
+        backtrack(digits,0,"");
+        
+       
 
-        backtrack(result, new StringBuilder(), digits, 0);
-        return result;
-    }
-
-    private void backtrack(List<String> result, StringBuilder current, String digits, int index) {
-        if (index == digits.length()) {
-            result.add(current.toString());
+    return ans;}
+    private void backtrack(String digits,int index,String cur){
+        if(index == digits.length()){
+            ans.add(cur);
             return;
         }
+        for(char ch:map.get(digits.charAt(index)-'0')){
 
-        String letters = phoneMap.get(digits.charAt(index));
-        for (char letter : letters.toCharArray()) {
-            current.append(letter);
-            backtrack(result, current, digits, index + 1);
-            current.deleteCharAt(current.length() - 1); 
+            backtrack(digits,index+1,cur+ch);
         }
     }
 }
