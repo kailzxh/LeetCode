9f2e0b8fa79c69ff0bd6a27b7a8e49a26aff887c
Kailash <godarakailash741@gmail.com>
2026-06-04 11:53:00 +0530

    Time: 1 ms (99.87%), Space: 64 MB (94.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,20 +1,51 @@
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int[] out= new int[nums.length];
+//         for(int i=0;i<nums.length;i++){
+//         int l=0;
+//         int r=nums.length-1;
+//             int sum=1;
+//             while(l<i || r>i){
+//                 if(l<i && r>i){
+//                     sum*=nums[l];
+//                     sum*=nums[r];
+//                     l++;
+//                     r--;
+//                 }else if(r>i){
+//                     sum*=nums[r];
+//                     r--;
+
+//                 }else{
+//                     sum*=nums[l];
+//                     l++;
+//                 }
+//             }
+//             out[i]=sum;
+//         }
+//     return out;}
+// }
+// time optimization
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] answer = new int[n];
-        int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            answer[i] = prefix;
-            prefix *= nums[i];
+     public int[] productExceptSelf(int[] nums) {
+        int presum=nums[0];
+        int[] arrpost = new int[nums.length];
+        int total=1;
+        for(int i=nums.length-1;i>=0;i--){
+            total*=nums[i];
+            arrpost[i]=total;
         }
+        int[] out = new int[nums.length];
+       
+            out[0]=arrpost[1];
 
-      
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;
-            suffix *= nums[i];
+        
+        for(int i=1;i<nums.length;i++){
+            if(nums.length-1==i){
+                out[i]=presum;
+            }else{
+                out[i]=presum*arrpost[i+1];
+                presum*=nums[i];
+            }
         }
-
-        return answer;
-    }
+    return out; }
 }
